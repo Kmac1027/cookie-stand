286498f770cf092f6b92ff6098922bfdd2c01bec
Kevin McNeil <dogwoodrawks@gmail.com>
2020-08-10 12:54:26 -0700

    added multiple files

---
+++ Javascript/app.js
@@ -0,0 +1,31 @@
+'use strict'
+
+var seattle = {
+  minPeople: 23,
+  maxPeople: 65,
+  avgCookie: 6.3
+}
+
+var tokyo = {
+  minPeople: 3,
+  maxPeople: 24,
+  avgCookie: 1.2
+}
+
+var dubai = {
+  minPeople: 11,
+  maxPeople: 38,
+  avgCookie: 3.7
+}
+
+var paris = {
+  minPeople: 20,
+  maxPeople: 38,
+  avgCookie: 2.3
+}
+
+var lima = {
+  minPeople: 2,
+  maxPeople: 16,
+  avgCookie: 4.6
+}
